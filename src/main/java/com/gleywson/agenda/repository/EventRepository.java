72d32dfd4d1e38274e0e6572419a6a27df7a9cc6
Gleywson Ribeiro <148725221+gleywson-softsy@users.noreply.github.com>
2025-02-08 11:06:54 -0300

    Criação dos respositórios de User e Event

---
+++ src/main/java/com/gleywson/agenda/repository/EventRepository.java
@@ -0,0 +1,11 @@
+package com.gleywson.agenda.repository;
+
+
+import com.gleywson.agenda.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByUserId(Long userId);
+}
